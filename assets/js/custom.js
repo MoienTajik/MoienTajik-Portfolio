d57b76fffa73e123029250fb5f4fab10f7c4307d
Moien Tajik <moientajik@yahoo.com>
2017-10-24 12:01:46 +0330

    minify js files

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,1 @@
-// Custom Scripts for Primal Template //
-
-jQuery(function ($) {
-    /* --------- Wow Init ------ */
-
-    new WOW().init();
-
-    /*----- Preloader ----- */
-
-    $(window).load(function () {
-        setTimeout(function () {
-            $('#loading').fadeOut('slow', function () {});
-        }, 3000);
-    });
-});
+jQuery(function($){new WOW().init();$(window).load(function(){setTimeout(function(){$('#loading').fadeOut('slow',function(){});},3000);});});
